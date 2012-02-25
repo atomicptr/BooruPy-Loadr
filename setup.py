f04e3c12539b72d9150c9d4256390cc35f121079
Kasoki <ikasoki@gmail.com>
2012-02-25 02:58:56 +0100

    added experimental setup.py for v1.2

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+	name = "BooruPy Loadr",
+	version = "1.2",
+	keywords = ['pictures', 'anime', 'booru', 'gelbooru', 'danbooru', 'image',
+		'board', 'imageboard', 'download', 'tool']
+	author = 'Christopher Kaster',
+	author_email = 'ikasoki@gmail.com',
+	maintainer = 'Christopher Kaster',
+	maintainer_email = 'ikasoki@gmail.com',
+	packages = ['BooruPy'],
+	url = 'http://github.com/Kasoki/BooruPy-Loadr',
+	license = 'GNU GENERAL PUBLIC LICENSE v3',
+	description = 'BooruPy Loadr is a download tool for various image board "Booru" systems.',
+	long_description = open('README.rst').read(),
+	platforms = 'OS Independent',
+	classifiers = [
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: End Users/Desktop',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2.7',
+		'Topic :: Internet'
+	]
+)
